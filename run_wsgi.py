21dd547091ba43e438dcd90144209eaf38bfce6e
lazyturtleas <lazyturtleasasin@gmail.com>
2025-09-07 14:20:35 +0300

    убрал старый код из запуска на 8080

--- run_wsgi.py
+++ run_wsgi.py
@@ -3,12 +3,7 @@ from randomquotes.wsgi import application
 import socket
 
 if __name__ == "__main__":
-    sock = socket.socket()
-    sock.bind(('', 0))
     PORT = 8080
-    sock.close()
-
     print(f"Starting WSGI server on http://127.0.0.1:{PORT}")
-
     with make_server('127.0.0.1', PORT, application) as httpd:
         httpd.serve_forever()
